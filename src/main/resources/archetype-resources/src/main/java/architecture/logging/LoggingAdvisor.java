d72975458381fda7d075fce8cce4b3189434888e
dperezcabrera <dperezcabrera@david-home>
2019-02-17 11:34:35 +0100

    Removed unused import

--- src/main/resources/archetype-resources/src/main/java/architecture/logging/LoggingAdvisor.java
+++ src/main/resources/archetype-resources/src/main/java/architecture/logging/LoggingAdvisor.java
@@ -12,7 +12,6 @@ import org.aopalliance.aop.Advice;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.support.AbstractPointcutAdvisor;
 import org.springframework.aop.support.StaticMethodMatcherPointcut;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
